60c8aa67e28848d55a954acf20b8f61cbad3f769
shenkao <Shen@SKC-MacBook.local>
2018-07-29 01:24:08 +0200

    change link text

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Learn to design and code React apps</h1>
     <p>Cool copy change here</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Watch the video here</Link>
+    <Link to="/page-2/">Watch the video</Link>
   </div>
 )
 
