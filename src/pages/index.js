5f3c8c1f8e57c637265ab7b12fe4d3180fd3ddc2
shenkao <Shen@SKC-MacBook.local>
2018-07-29 01:19:56 +0200

    added directory

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to design and code React apps</h1>
+    <p>Cool copy change here</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
